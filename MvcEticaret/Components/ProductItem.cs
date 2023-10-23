c051bc9de671199537e0f81a80b386146c55bc2d
sevvalydrrm <ysevvalyldrm@gmail.com>
2023-10-23 15:20:04 +0300

    Add project files.

---
+++ MvcEticaret/Components/ProductItem.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcEticaret.Components
+{
+	public class ProductItem : ViewComponent
+	{
+		public IViewComponentResult Invoke(string title, int starCount, decimal oldPrice, decimal price, string imageUrl, bool isSale)
+        {
+            var model = new ProductItemModel
+            {
+                Title = title,
+                StarCount = starCount,
+                OldPrice = oldPrice,
+                Price = price,
+                ImageUrl = imageUrl,
+                IsSale = isSale
+            };
+            
+            return View(model);
+        }
+	}
+}
---
+++ MvcEticaret/Controllers/HomeController.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcEticaret.Components;
+using MvcEticaret.Models;
+using System.Diagnostics;
+
+namespace MvcEticaret.Controllers
+{
+	public class HomeController : Controller
+	{
+		//method oluşturduk
+		private List<ProductItemModel> GetSampleProducts()
+		{
+			var products = new List<ProductItemModel>
+		{
+			new ProductItemModel
+		{
+			Title = "Fancy Product",
+			StarCount = 0,
+			OldPrice = 19.99m,
+			Price = 39.99m,
+			ImageUrl = "https://st-thenorthface.mncdn.com/Content/media/ProductImg/original/638272004368117679.jpg",
+			IsSale = false
+		},
+			new ProductItemModel
+		{
+			Title = "Special Item",
+			StarCount = 5,
+			OldPrice = 29.99m,
+			Price = 19.99m,
+			ImageUrl = "https://st-thenorthface.mncdn.com/Content/media/ProductImg/original/638154059507771574.jpg",
+			IsSale = true
+		},
+				new ProductItemModel
+		{
+			Title = "Sale Item",
+			StarCount = 0,
+			OldPrice = 39.99m,
+			Price = 29.99m,
+			ImageUrl = "https://st-thenorthface.mncdn.com/Content/media/ProductImg/original/638271992678926764.jpg",
+			IsSale = true
+		},
+				new ProductItemModel
+		{
+			Title = "Popular Item",
+			StarCount = 4,
+			OldPrice = 49.99m,
+			Price = 39.99m,
+			ImageUrl = "https://st-thenorthface.mncdn.com/Content/media/ProductImg/original/636734978447063571.jpg",
+			IsSale = false
+		},
+				new ProductItemModel
+		{
+			Title = "Sale Item",
+			StarCount = 0,
+			OldPrice = 19.99m,
+			Price = 9.99m,
+			ImageUrl = "https://st-thenorthface.mncdn.com/Content/media/ProductImg/original/638271027307556807.jpg",
+			IsSale = true
+		},
+				new ProductItemModel
+		{
+			Title = "Fancy Product",
+			StarCount = 0,
+			OldPrice = 29.99m,
+			Price = 59.99m,
+			ImageUrl ="https://st-thenorthface.mncdn.com/Content/media/ProductImg/original/638304374276601201.jpg",
+			IsSale = false
+		},
+				new ProductItemModel
+		{
+			Title = "Special Item",
+			StarCount = 5,
+			OldPrice = 59.99m,
+			Price = 39.99m,
+			ImageUrl = "https://st-thenorthface.mncdn.com/Content/media/ProductImg/original/638029167052281948.jpg",
+			IsSale = true
+		},
+				new ProductItemModel
+		{
+			Title = "Popular Item",
+			StarCount = 5,
+			OldPrice = 69.99m,
+			Price = 89.99m,
+			ImageUrl = "https://st-thenorthface.mncdn.com/Content/media/ProductImg/original/638029172066220024.jpg",
+			IsSale = false
+		}
+	    };
+			return products;
+		}
+
+		private readonly ILogger<HomeController> _logger;
+
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
+
+		//methodu index'e gönderdik
+		public IActionResult Index()
+		{
+			var products = GetSampleProducts();
+			return View(products);
+		}
+
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
